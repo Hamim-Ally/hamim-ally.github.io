a9d21b90c09acfdb76cf23bfe677f906b99dd809
Hamim Ally <hamimally3105@gmail.com>
2025-04-17 23:40:23 +0600

    commit

---
+++ Assets/x.r
@@ -0,0 +1,49 @@
+
+    nude, nudity, naked, sex, intercourse, porn, porno, xxx, nsfw, adult,
+    boobs, tits, nipples, cleavage, underboob, areola, nipple, pussy, vagina,
+    penis, cock, dick, cum, sperm, orgasm, masturbate, masturbation, fetish,
+    blowjob, handjob, rimjob, anal, doggystyle, 69, missionary, bj, deepthroat,
+    butt, ass, asshole, threesome, foursome, hardcore, softcore, milf, teen,
+    virgin, escort, strip, stripper, nudist, incest, bdsm, bondage, dominatrix,
+    erotic, erotica, lingerie, kinky, kink, sex toy, dildo, vibrator, lube,
+    clit, clitoris, moan, groan, panties, thong, sex tape, sex scene, camgirl,
+    camwhore, onlyfans, fleshlight, cumshot, facial, creampie, squirt, pegging,
+
+
+    slut, whore, skank, hoe, thot, tramp, hooker, slag, bimbo, nympho,
+    horny, fuck, fucked, fucking, shit, bullshit, bitch, bastard, douche,
+    douchebag, piss, pissed, arse, retard, retarded, moron, dumbass, dipshit,
+    jackass, asswipe, asshat, prick, jerkoff, wanker, tosser, bollocks,
+
+
+    footjob, foot fetish, latex fetish, gag, leash, choke, spank, spanking,
+    cuck, cuckold, voyeur, exhibitionist, dominant, submissive, sub, dom,
+    slave, master, roleplay, wet dream, tentacle, hentai, yaoi, yuri, futa,
+    futanari, incel, loli, shota, furry, yiff, nsfw furry, erotic furry,
+
+
+    hot girl, sexy woman, sexy teen, tight body, busty, curvy, big boobs, big ass,
+    naughty, bad girl, girl next door, wet, sweaty, provocative, suggestive, revealing,
+    exposed, lewd, sultry, tempting, teasing, dirty, explicit,
+
+    
+    onlyfans, xvideos, redtube, pornhub, rule34, camshow, livecam, snapnudes, nsfwart,
+    sexcam, nudes, sext, sexting, smut, leak, leaked nudes, deepnude, uncensored
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+deformed, poorly drawn, extra limbs, blurry, ugly, tiling, bad anatomy, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, cropped, body out of frame, double face, mutated, cloned face, mutation, missing limbs, extra fingers, fused fingers, conjoined fingers, malformed limbs, disjointed limbs, floating limbs, long neck, long body, distorted, disproportionate, ugly eyes, imperfect eyes, skewed eyes, unnatural face, stiff face, unnatural body, unbalanced body, details are not clear, low detail, sticky details, low resolution, low quality, normal quality, jpeg artifacts, watermark, signature, username, artist name, text, logo, error, oversaturated, grainy, noisy, bad composition, bad lighting, plastic, doll-like, static, boring, cropped, worst quality, low quality, normal quality, mutated hands and fingers, long body, deformed, distorted, disfigured, cartoon, anime, toon, unrealistic, unreal, painting by bad-artist-anime, bad 3D render, bad 3D shadowing, worst hand, bad foot, extra leg, missing hand, disappearing limbs, adult, mature, curvy, plump, fat, strabismus, bad angle, crappy infographic, educational cartoon, reference sheet, multiple view, visible UI, clothing cutout, side slit, holding camera, vaginas in breasts
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,85 +2,6 @@ $(function () {
 
   "use strict";
 
-  const options = {
-    containers: ["#swup", "#swupMenu"],
-    plugins: [new SwupHeadPlugin()],
-    animateHistoryBrowsing: true,
-    linkSelector: 'a:not([data-no-swup])'
-  };
-  const swup = new Swup(options);
-
-
-
-  // page loading
-  $(document).ready(function () {
-    anime({
-      targets: '.art-preloader .art-preloader-content',
-      opacity: [0, 1],
-      delay: 200,
-      duration: 600,
-      easing: 'linear',
-      complete: function (anim) {
-
-      }
-    });
-    anime({
-      targets: '.art-preloader',
-      opacity: [1, 0],
-      delay: 2200,
-      duration: 400,
-      easing: 'linear',
-      complete: function (anim) {
-        $('.art-preloader').css('display', 'none');
-      }
-    });
-  });
-
-  var bar = new ProgressBar.Line(preloader, {
-    strokeWidth: 1.7,
-    easing: 'easeInOut',
-    duration: 1400,
-    delay: 750,
-    trailWidth: 1.7,
-    svgStyle: {
-      width: '100%',
-      height: '100%'
-    },
-    step: (state, bar) => {
-      bar.setText(Math.round(bar.value() * 100) + ' %');
-    }
-  });
-
-  bar.animate(1);
-
-  // counters
-  anime({
-    targets: '.art-counter-frame',
-    opacity: [0, 1],
-    duration: 800,
-    delay: 2300,
-    easing: 'linear',
-  });
-
-  anime({
-    targets: '.art-counter',
-    delay: 1300,
-    opacity: [1, 1],
-    complete: function (anim) {
-      $('.art-counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-          Counter: $(this).text()
-        }, {
-          duration: 2000,
-          easing: 'linear',
-          step: function (now) {
-            $(this).text(Math.ceil(now));
-          }
-        });
-      });
-    }
-  });
-
   // Contact form
   $('.art-input').keyup(function () {
     if ($(this).val()) {
@@ -305,212 +226,212 @@ $(function () {
   });
 
   // reinit
-  swup.hooks.on('page:view', () => {
+  // swup.hooks.on('page:view', () => {
 
-    $("#form").submit(function () {
-      $.ajax({
-        type: "POST",
-        url: "mail.php",
-        data: $(this).serialize()
-      }).done(function () {
+  //   $("#form").submit(function () {
+  //     $.ajax({
+  //       type: "POST",
+  //       url: "mail.php",
+  //       data: $(this).serialize()
+  //     }).done(function () {
 
-        var tl = anime.timeline({
-          easing: 'easeOutExpo',
-        });
+  //       var tl = anime.timeline({
+  //         easing: 'easeOutExpo',
+  //       });
 
-        tl
-          .add({
-            targets: '.art-submit',
-            opacity: 0,
-            scale: .5,
-          })
-          .add({
-            targets: '.art-success',
-            scale: 1,
-            height: '45px',
-          })
-      });
-      return false;
-    });
+  //       tl
+  //         .add({
+  //           targets: '.art-submit',
+  //           opacity: 0,
+  //           scale: .5,
+  //         })
+  //         .add({
+  //           targets: '.art-success',
+  //           scale: 1,
+  //           height: '45px',
+  //         })
+  //     });
+  //     return false;
+  //   });
 
-    // Masonry Grid
-    $('.art-grid').isotope({
-      filter: '*',
-      itemSelector: '.art-grid-item',
-      transitionDuration: '.6s',
-    });
+  //   // Masonry Grid
+  //   $('.art-grid').isotope({
+  //     filter: '*',
+  //     itemSelector: '.art-grid-item',
+  //     transitionDuration: '.6s',
+  //   });
 
-    $('.art-filter a').on('click', function () {
-      $('.art-filter .art-current').removeClass('art-current');
-      $(this).addClass('art-current');
+  //   $('.art-filter a').on('click', function () {
+  //     $('.art-filter .art-current').removeClass('art-current');
+  //     $(this).addClass('art-current');
 
-      var selector = $(this).data('filter');
-      $('.art-grid').isotope({
-        filter: selector
-      });
-      return false;
-    });
+  //     var selector = $(this).data('filter');
+  //     $('.art-grid').isotope({
+  //       filter: selector
+  //     });
+  //     return false;
+  //   });
 
-    anime({
-      targets: '.art-counter-frame',
-      opacity: [0, 1],
-      duration: 800,
-      delay: 300,
-      easing: 'linear',
-    });
+  //   anime({
+  //     targets: '.art-counter-frame',
+  //     opacity: [0, 1],
+  //     duration: 800,
+  //     delay: 300,
+  //     easing: 'linear',
+  //   });
 
-    $('.art-counter').each(function () {
-      $(this).prop('Counter', 0).animate({
-        Counter: $(this).text()
-      }, {
-        duration: 2000,
-        easing: 'linear',
-        step: function (now) {
-          $(this).text(Math.ceil(now));
-        }
-      });
-    });
+  //   $('.art-counter').each(function () {
+  //     $(this).prop('Counter', 0).animate({
+  //       Counter: $(this).text()
+  //     }, {
+  //       duration: 2000,
+  //       easing: 'linear',
+  //       step: function (now) {
+  //         $(this).text(Math.ceil(now));
+  //       }
+  //     });
+  //   });
 
-    // slider testimonials
-    var swiper = new Swiper('.art-testimonial-slider', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      speed: 1400,
-      autoplay: false,
-      autoplaySpeed: 5000,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.art-testi-swiper-next',
-        prevEl: '.art-testi-swiper-prev',
-      },
-      breakpoints: {
-        1500: {
-          slidesPerView: 2,
-        },
-        1200: {
-          slidesPerView: 2,
-        },
-        992: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 1,
-        }
-      },
-    });
+  //   // slider testimonials
+  //   var swiper = new Swiper('.art-testimonial-slider', {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //     speed: 1400,
+  //     autoplay: false,
+  //     autoplaySpeed: 5000,
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: '.art-testi-swiper-next',
+  //       prevEl: '.art-testi-swiper-prev',
+  //     },
+  //     breakpoints: {
+  //       1500: {
+  //         slidesPerView: 2,
+  //       },
+  //       1200: {
+  //         slidesPerView: 2,
+  //       },
+  //       992: {
+  //         slidesPerView: 2,
+  //       },
+  //       768: {
+  //         slidesPerView: 1,
+  //       }
+  //     },
+  //   });
 
-    // slider works
-    var swiper = new Swiper('.art-works-slider', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      speed: 1400,
-      autoplay: {
-        delay: 4000,
-      },
-      autoplaySpeed: 5000,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.art-works-swiper-next',
-        prevEl: '.art-works-swiper-prev',
-      },
-      breakpoints: {
-        1500: {
-          slidesPerView: 3,
-        },
-        1200: {
-          slidesPerView: 3,
-        },
-        992: {
-          slidesPerView: 1,
-        },
-      },
-    });
+  //   // slider works
+  //   var swiper = new Swiper('.art-works-slider', {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //     speed: 1400,
+  //     autoplay: {
+  //       delay: 4000,
+  //     },
+  //     autoplaySpeed: 5000,
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: '.art-works-swiper-next',
+  //       prevEl: '.art-works-swiper-prev',
+  //     },
+  //     breakpoints: {
+  //       1500: {
+  //         slidesPerView: 3,
+  //       },
+  //       1200: {
+  //         slidesPerView: 3,
+  //       },
+  //       992: {
+  //         slidesPerView: 1,
+  //       },
+  //     },
+  //   });
 
-    // slider blog
-    var swiper = new Swiper('.art-blog-slider', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      speed: 1400,
-      autoplay: {
-        delay: 4000,
-      },
-      autoplaySpeed: 5000,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.art-blog-swiper-next',
-        prevEl: '.art-blog-swiper-prev',
-      },
-      breakpoints: {
-        1500: {
-          slidesPerView: 3,
-        },
-        1200: {
-          slidesPerView: 3,
-        },
-        992: {
-          slidesPerView: 1,
-        },
-      },
-    });
+  //   // slider blog
+  //   var swiper = new Swiper('.art-blog-slider', {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //     speed: 1400,
+  //     autoplay: {
+  //       delay: 4000,
+  //     },
+  //     autoplaySpeed: 5000,
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: '.art-blog-swiper-next',
+  //       prevEl: '.art-blog-swiper-prev',
+  //     },
+  //     breakpoints: {
+  //       1500: {
+  //         slidesPerView: 3,
+  //       },
+  //       1200: {
+  //         slidesPerView: 3,
+  //       },
+  //       992: {
+  //         slidesPerView: 1,
+  //       },
+  //     },
+  //   });
 
-    $('[data-fancybox="gallery"]').fancybox({
-      animationEffect: "zoom-in-out",
-      animationDuration: 600,
-      transitionDuration: 1200,
-      buttons: [
-        "zoom",
-        "slideShow",
-        "thumbs",
-        "close"
-      ],
-    });
+  //   $('[data-fancybox="gallery"]').fancybox({
+  //     animationEffect: "zoom-in-out",
+  //     animationDuration: 600,
+  //     transitionDuration: 1200,
+  //     buttons: [
+  //       "zoom",
+  //       "slideShow",
+  //       "thumbs",
+  //       "close"
+  //     ],
+  //   });
 
-    $('[data-fancybox="diplome"]').fancybox({
-      animationEffect: "zoom-in-out",
-      animationDuration: 600,
-      transitionDuration: 1200,
-      buttons: [
-        "zoom",
-        "slideShow",
-        "thumbs",
-        "close",
-      ],
-    });
+  //   $('[data-fancybox="diplome"]').fancybox({
+  //     animationEffect: "zoom-in-out",
+  //     animationDuration: 600,
+  //     transitionDuration: 1200,
+  //     buttons: [
+  //       "zoom",
+  //       "slideShow",
+  //       "thumbs",
+  //       "close",
+  //     ],
+  //   });
 
-    $('[data-fancybox="recommendation"]').fancybox({
-      animationEffect: "zoom-in-out",
-      animationDuration: 600,
-      transitionDuration: 1200,
-      buttons: [
-        "zoom",
-        "slideShow",
-        "thumbs",
-        "close",
-      ],
-    });
+  //   $('[data-fancybox="recommendation"]').fancybox({
+  //     animationEffect: "zoom-in-out",
+  //     animationDuration: 600,
+  //     transitionDuration: 1200,
+  //     buttons: [
+  //       "zoom",
+  //       "slideShow",
+  //       "thumbs",
+  //       "close",
+  //     ],
+  //   });
 
-    $.fancybox.defaults.hash = false;
+  //   $.fancybox.defaults.hash = false;
 
-    $('.current-menu-item a').clone().prependTo('.art-current-page');
+  //   $('.current-menu-item a').clone().prependTo('.art-current-page');
 
 
 
-    $('.menu-item').on('click', function () {
-      if ($(this).hasClass('menu-item-has-children')) {
-        $(this).children('.sub-menu').toggleClass('art-active');
-      } else {
-        $('.art-menu-bar-btn , .art-menu-bar , .art-info-bar , .art-content , .art-menu-bar-btn , .art-info-bar-btn').removeClass('art-active , art-disabled');
-      }
-    });
-  })
+  //   $('.menu-item').on('click', function () {
+  //     if ($(this).hasClass('menu-item-has-children')) {
+  //       $(this).children('.sub-menu').toggleClass('art-active');
+  //     } else {
+  //       $('.art-menu-bar-btn , .art-menu-bar , .art-info-bar , .art-content , .art-menu-bar-btn , .art-info-bar-btn').removeClass('art-active , art-disabled');
+  //     }
+  //   });
+  // })
 
 });
